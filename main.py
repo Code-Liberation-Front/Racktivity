2b7ea0923ed2270ed2a3211f55c077510104bd3a
Mobro <39844165+moyito2604@users.noreply.github.com>
2024-02-01 15:36:19 -0500

    Basic Menu Added

---
+++ main.py
@@ -0,0 +1,14 @@
+
+while True:
+    print("______              _     _    _         _  _          \n"
+          "| ___ \            | |   | |  (_)       (_)| |         \n"
+          "| |_/ / __ _   ___ | | __| |_  _ __   __ _ | |_  _   _ \n"
+          "|    / / _` | / __|| |/ /| __|| |\ \ / /| || __|| | | |\n"
+          "| |\ \| (_| || (__ |   < | |_ | | \ V / | || |_ | |_| |\n"
+          "\_| \_|\__,_| \___||_|\_\ \__||_|  \_/  |_| \__| \__, |\n"
+          "                                                  __/ |\n"
+          "                                                 |___/ \n")
+    print("Please select an option:\n\n1: Create a new table for your rack\n2: Remove a rack table"
+          "\n3: Add new rows to your rack\n4: Remove a row of information from a rack\n5: Input a single server's "
+          "information\n6: Display a single Server's information\n7: Import from a CSV file")
+    input()
